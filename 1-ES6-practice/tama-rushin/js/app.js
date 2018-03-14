4f083ba07cf400557b363a5ea96504d905f2da86
tamarushin <tamarushin@gmail.com>
2018-03-14 11:53:50 -0700

    created template literals

--- 1-ES6-practice/tama-rushin/js/app.js
+++ 1-ES6-practice/tama-rushin/js/app.js
@@ -16,7 +16,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg;`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -35,7 +35,7 @@ function displayPics(){
     }
   }
   console.log(rando);
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
+  // Done: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
   //variables declared with let are not accessible if they are declared in their enclosing function block.
   console.log(viewed);
 
@@ -62,7 +62,7 @@ function handleClick(event) {
   for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -73,7 +73,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     let liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} has + ${allProducts[i].votes} votes in ${allProducts[i].views}  views`;
     list.appendChild(liEl);
   }
 }
@@ -125,7 +125,7 @@ document.getElementById('bus').addEventListener('click', function(){
 
 if(localStorage.busmall){
   console.log('Local storage data exists');
-  allProducts = JSON.parse(localStorage.busmall)
+  allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
   for(let i = 0; i < names.length; i++) {
